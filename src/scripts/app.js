ba02692f9c5e38a570187a8b23029691b0518cdf
Xiquad <xiquad@gmail.com>
2017-06-30 23:20:17 +0300

    few mods

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,15 @@
 $(document).ready(function(){
+
 	let all = $('.blocks').children();
-	let first = $('.blocks').children().first();
-	let last = $('.blocks').children().last();
-	
-	// $(first).height($(window).height());
-	// $(last).height($(window).height());
 
 	$(all).each((index, item)=>{
-		$(item).css({'min-height':$(window).height()});
+		$(item).css({'min-height':$(window).height()+'px'});
 	});
-});
 
-$(document).ready(function(){
 	$('.grid-speakers').slick({
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 3
 	});
+
 });

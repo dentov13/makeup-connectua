6e9c9b97e7e66338413162c17a5fe7764a0e1d1d
dentov13 <tovstokhatko.denis@gmail.com>
2017-07-02 21:08:58 +0300

    speakers refactoring and fixed mobile version

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -29,25 +29,37 @@ $(document).ready(function(){
 	// 	}]
 	// });
 
-	$(window).on('scroll', () => {
-
-	    let scrollTop = $(this).scrollTop(),
-	    	elem = $('.nav-bottom'),
-	    	elemHeight = elem.outerHeight(),
-	    	topDistance = $('.nav-bottom').offset().top,
-	    	blockHeight = $('.first').height();
-
-        if (topDistance < scrollTop ) {
-        	if(!elem.hasClass('fixable')) 
-        		elem.addClass('fixable');
-        }
-
-        if (scrollTop < (blockHeight-elemHeight)) {
-        	if(elem.hasClass('fixable')) 
-        		elem.removeClass('fixable');
-        }
-
-	});
+	// $(window).on('scroll', () => {
+	//
+	//     let scrollTop = $(this).scrollTop(),
+	//     	elem = $('.nav-bottom'),
+	//     	elemHeight = elem.outerHeight(),
+	//     	topDistance = $('.nav-bottom').offset().top,
+	//     	blockHeight = $('.first').height();
+	//
+  //       if (topDistance < scrollTop ) {
+  //       	if(!elem.hasClass('fixable'))
+  //       		elem.addClass('fixable');
+  //       }
+	//
+  //       if (scrollTop < (blockHeight-elemHeight)) {
+  //       	if(elem.hasClass('fixable'))
+  //       		elem.removeClass('fixable');
+  //       }
+	//
+	// });
+  $(".nav-top").on("click","a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
+  $(".nav-bottom").on("click","a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 
 	function updateHeight(){
 		for(let i=0; i<all.length; i++){
